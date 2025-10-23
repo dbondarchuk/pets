e9f54c50f80683b231c58ae56b3adc9273052610
Dmytro Bondarchuk <dbondarchuk@captura.io>
2025-10-23 11:38:51 -0400

    Fix text for sign in page

--- src/features/auth/components/sigin-view.tsx
+++ src/features/auth/components/sigin-view.tsx
@@ -47,7 +47,7 @@ export default async function SignInViewPage() {
         <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
           <div className='flex flex-col space-y-2 text-center'>
             <h1 className='text-2xl font-semibold tracking-tight'>
-              {t('create_account')}
+              {t('sign_in')}
             </h1>
             <p className='text-sm text-muted-foreground'>
               {t('enter_credentials')}
